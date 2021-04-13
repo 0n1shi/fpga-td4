1813678358df222da04c1124b85747eedd7b975b
0n1shi <41312202+0n1shi@users.noreply.github.com>
2021-04-13 21:29:24 +0900

    fix test module

--- test/test_fpga_computer.sv
+++ test/test_fpga_computer.sv
@@ -1,8 +1,17 @@
 module test_fpga_computer;
 
-logic clk, led, key;
+logic clk, key, led;
+logic [3:0] switch;
+logic [6:0] hex0, hex1;
 
-fpga_computer fpga_computer(.CLK(clk), .KEY(key), .LEDR(led));
+fpga_computer fpga_computer(
+    .CLK(clk), 
+    .KEY(key), 
+    .SW(switch),
+    .LEDR(led),
+    .HEX0(hex0),
+    .HEX1(hex1)
+);
 
 parameter OSC100M_PERIOD = 10000; // ps
 
@@ -10,3 +19,5 @@ initial begin
     clk = 1'b0;
     forever #(OSC100M_PERIOD / 2) clk <= ~clk;
 end
+
+endmodule
